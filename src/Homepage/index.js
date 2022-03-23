8d7b10e4a0d5f0effbd533eebc7fb3795eb04b4b
Dao Vinh Linh <linh142000@gmail.com>
2022-03-24 01:48:05 +0700

    Fix css

--- src/Homepage/index.js
+++ src/Homepage/index.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { Banner } from '../components/Banner';
 import Navbar from '../components/Navbar';
 
-const line_1 = '....'
+const line_1 = '....';
 
 function Homepage() {
-    return (
-        <div>
-            <Navbar />
-            <Banner />
-        </div >
-    );
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+    </div>
+  );
 }
 export default Homepage;
